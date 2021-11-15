097b9fbf844b1f84965b80abd471ae145297aad4
arshith787 <82289984+arshith787@users.noreply.github.com>
2021-11-14 20:15:01 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,25 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
-	
-}
+var engine, world;
 
 function setup() {
-	createCanvas(800, 700);
+   createCanvas(800, 700);
 
 
-	engine = Engine.create();
-	world = engine.world;
+   engine = Engine.create();
+   world = engine.world;
 
 	//Create the Bodies Here.
+  paper = new Paper(100, 600, 10);
 
+  ground = new Ground(400, 680, 800, 20);
+  
+  leftSide = new Dustbin(550, 620, 20, 100);
+  bottom = new Dustbin(610, 660, 100, 20);
+  rightSide = new Dustbin(670, 620, 20, 100);
 
 	Engine.run(engine);
   
@@ -26,11 +28,25 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
+
   background(0);
   
+  Engine.update(engine);
+
+  paper.display();
+
+  ground.display();
+
+  leftSide.display();
+  bottom.display();
+  rightSide.display();
+
   drawSprites();
  
 }
 
-
+function keyPressed(){
+  if(keyCode === UP_ARROW) {
+    Matter.Body.applyForce(paper.body, paper.body.position, {x:15, y: -15})
+  }
 
