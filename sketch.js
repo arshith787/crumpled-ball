893db3cd3de95d7abda1e74f63acfdc9115bda92
arshith787 <82289984+arshith787@users.noreply.github.com>
2021-11-27 19:49:13 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,51 +2,51 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
-var engine, world;
-
-function setup() {
-   createCanvas(800, 700);
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject,engine;
+var world;
 
 
-   engine = Engine.create();
-   world = engine.world;
+function setup(){
+	createCanvas(1200, 400);
+	rectMode(CENTER);
+    
 
-  paper = new Paper(100, 600, 10);
+	engine = Engine.create();
+	world = engine.world;
+	paperObject=new paper(40,40,20);
+	dustbinObj=new dustbin(1000,450);
+	groundObject=new ground(600,400);
+	
+	
 
-  ground = new Ground(400, 680, 800, 20);
-  
-  leftSide = new Dustbin(550, 620, 20, 100);
-  bottom = new Dustbin(610, 660, 100, 20);
-  rightSide = new Dustbin(670, 620, 20, 100);
+	
 
 	Engine.run(engine);
-  
+	
 }
 
 
-function draw() {
+function draw(){
   rectMode(CENTER);
-
-  background(0);
+  background(100);
+ 
   
-  Engine.update(engine);
-
-  paper.display();
-
-  ground.display();
-
-  leftSide.display();
-  bottom.display();
-  rightSide.display();
-
-  drawSprites();
+  paperObject.display();
+  dustbinObj.display();
+  groundObject.display();
+  
+  
+ 
+  
+  
  
 }
-
 function keyPressed(){
-	
-  if(keyCode === UP_ARROW) {
-    Matter.Body.applyForce(paper.body, paper.body.position, {x:15, y: -15})
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+	}
 }
-}
+
+
+
