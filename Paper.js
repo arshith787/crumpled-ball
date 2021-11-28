86371dc3317c68ac2a627a75897be2eadd271528
arshith787 <82289984+arshith787@users.noreply.github.com>
2021-11-27 19:57:17 -0500

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -1,22 +1,38 @@
-class Paper {
-  constructor(x, y, radius) {
-    var options = {
-      isStatic: false,
-      'restitution':0.3,
-      'friction':0.5,
-      'density':1.2
-    }
-    this.body = Bodies.circle(x, y, radius, options);
-    this.radius = radius
-    this.image = loadImage("paper.png");
-    
-    World.add(world, this.body);
-    }
-  display(){
-    ellipseMode(RADIUS);
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:100,
+			density:1.2
 
-    fill("yellow");
+			
+			}
+		this.r=r;
+		
+		this.image=loadImage("paper.png");
+		this.body=Bodies.circle(x, y, r, options)
+		World.add(world, this.body);
 
-    circle(this.body.position.x, this.body.position.y, this.radius);
-  }
-  };
+	}
+	display()
+	{
+			
+		var paperpos=this.body.position;		
+
+		push()
+		translate(paperpos.x, paperpos.y);
+		rectMode(CENTER)
+		//strokeWeight(4);
+		fill(255)
+		imageMode(CENTER);
+		
+		image(this.image, 0,0,2*this.r, 2*this.r)
+		//ellipse(0,0,2*this.r, 2*this.r)
+		
+		pop()
+	}
+
+}
