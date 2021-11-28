50a45e53686c25371941303650dc667fc847b305
arshith787 <82289984+arshith787@users.noreply.github.com>
2021-11-27 20:03:54 -0500

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -1,22 +1,45 @@
-class Dustbin {
-    constructor(x, y, width, height) {
-      var options = {
-          isStatic: true,
-          'restitution':0,
-          'friction':1,
-          'density':0.1
-      }
-      this.body = Bodies.rectangle(x, y, width, height, options);
-      this.width = width;
-      this.height = height;
-      
-      World.add(world, this.body);
-    }
-    display(){
-      rectMode(CENTER);
+class dustbin
+{
+	constructor(x,y)
+	{
+		var options={
+			isStatic:true,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
 
-      fill(255);
+			
+			}
+		
+        this.leftbody=Bodies.rectangle(x-100+10+40, y-60-10-50,20,100, options)
+        this.rightbody=Bodies.rectangle(x+100-40, y-60-10-50,20,100, options)
+        this.centerbody=Bodies.rectangle(x, y-60,100,20, options)
+        this.image=loadImage("dustbingreen.png");
+        World.add(world, this.leftbody);
+        World.add(world, this.rightbody);
+        World.add(world, this.centerbody);
 
-      rect(this.body.position.x, this.body.position.y, this.width, this. height);
-    }
-  };
+	}
+	display()
+	{
+			
+				
+
+			
+		push();
+		rectMode(CENTER)
+		
+		fill(255)
+        imageMode(CENTER);
+           
+        image(this.image,this.centerbody.position.x,this.centerbody.position.y-50,100, 100)
+        rect(this.centerbody.position.x,this.centerbody.position.y,100,20)
+        rect(this.leftbody.position.x,this.leftbody.position.y,20,100)
+        rect(this.rightbody.position.x,this.rightbody.position.y,20,100)
+
+
+		pop();
+			
+	}
+
+}
